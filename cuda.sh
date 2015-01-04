0f6a410165c32ee934426ad1e13285292ce669e0
Gurov D.A. <guranvirnew@yandex.ru>
2015-01-04 13:55:24 +0300

    Create cuda.sh
    install script

---
+++ cuda.sh
@@ -0,0 +1,8 @@
+sudo apt-get install binutils gcc make g++-4.6 build-essential linux-headers-amd64
+sudo apt-get install mpich2 
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_6.5-14_amd64.deb
+sudo apt-get update
+sudo apt-get install cuda
+export PATH=/usr/local/cuda-6.5/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-6.5/lib64:$LD_LIBRARY_PATH
+cuda-install-samples-6.5.sh cudaexamps
