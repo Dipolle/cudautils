1b06a855321c7c6bdc45a067cdc10020704b384c
Gurov D.A. <guranvirnew@yandex.ru>
2015-01-04 14:00:35 +0300

    Update cuda.sh

--- cuda.sh
+++ cuda.sh
@@ -1,6 +1,7 @@
 sudo apt-get install binutils gcc make g++-4.6 build-essential linux-headers-amd64
 sudo apt-get install mpich2 
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_6.5-14_amd64.deb
+dpkg -i cuda-repo-ubuntu1404_6.5-14_amd64.deb
 sudo apt-get update
 sudo apt-get install cuda
 export PATH=/usr/local/cuda-6.5/bin:$PATH
