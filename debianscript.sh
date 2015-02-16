841e6b6e6371e981a39d49fe9a3b971f4c9082bc
Gurov D.A. <guranvirnew@yandex.ru>
2015-02-17 02:31:19 +0300

    script for Debian

---
+++ debianscript.sh
@@ -0,0 +1,6 @@
+  aptitude purge xserver-xorg-video-nouveau
+  apt-get install freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libgl1-mesa-glx libglu1-mesa libglu1-mesa-dev
+  wget http://developer.download.nvidia.com/compute/cuda/6_5/rel/installers/cuda_6.5.14_linux_64.run
+  chmod +x cuda_6.0.37_linux_64.run
+  ./cuda_6.0.37_linux_64.run
+  
