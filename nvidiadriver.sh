3c7aaa5b987e35e4b66cebf978637f5664d14d61
Gurov D.A. <guranvirnew@yandex.ru>
2015-02-20 14:11:16 +0300

    Update nvidiadriver.sh

--- nvidiadriver.sh
+++ nvidiadriver.sh
@@ -1,4 +1,4 @@
 apt-get install module-assistant nvidia-kernel-common
  m-a auto-install nvidia-kernel${VERSION}-source
  apt-get install nvidia-glx${VERSION}
- cp -p /etc/X11/xorg.conf /etc/X11/xorg.conf.bak
+nano /etc/X11/xorg.conf
