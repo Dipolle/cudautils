a35820f0a645398e831102cc9ad8d13eeaec4356
Gurov D.A. <guranvirnew@yandex.ru>
2015-02-21 23:47:49 +0300

    Update intel.sh

--- intel.sh
+++ intel.sh
@@ -1,3 +1,4 @@
+apt-get remove --purge xserver-xorg-video-intel
 wget http://ftp.us.debian.org/debian/pool/main/x/xserver-xorg-video-intel/xserver-xorg-video-intel_2.99.917-1~exp1_amd64.deb
 dpkg -i *.deb
-#apt-get install xserver-xorg-video-intel
+
