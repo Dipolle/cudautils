dc6015695657c9d429bd7091ee86356fe9719eec
Gurov D.A. <guranvirnew@yandex.ru>
2015-02-21 23:37:47 +0300

    Update intel.sh

--- intel.sh
+++ intel.sh
@@ -1,1 +1,3 @@
-apt-get install xserver-xorg-video-intel
+wget http://ftp.us.debian.org/debian/pool/main/x/xserver-xorg-video-intel/xserver-xorg-video-intel_2.99.917-1~exp1_amd64.deb
+dpkg -i *.deb
+#apt-get install xserver-xorg-video-intel
