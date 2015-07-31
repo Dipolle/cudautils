1a479fb1b134bbd20b23119713a41c180587794f
Gurov D.A. <guranvirnew@yandex.ru>
2015-07-31 22:05:48 +0300

    Delete mate.sh

--- mate.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-add-repository ppa:ubuntu-mate-dev/ppa
-
-sudo apt-add-repository ppa:ubuntu-mate-dev/trusty-mate
-
-sudo apt-get update && sudo apt-get upgrade
-
-sudo apt-get install --no-install-recommends ubuntu-mate-core ubuntu-mate-desktop
