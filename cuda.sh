2aba439935e4fbf98f77c3c86fce721879d98092
Gurov D.A. <guranvirnew@yandex.ru>
2015-08-01 02:05:53 +0300

    Update cuda.sh

--- cuda.sh
+++ cuda.sh
@@ -1,2 +1,2 @@
-wget http://us.download.nvidia.com/XFree86/Linux-x86_64/352.30/NVIDIA-Linux-x86_64-352.30.run
-wget http://developer.download.nvidia.com/compute/cuda/7_0/Prod/local_installers/cuda_7.0.28_linux.run
+apt-get install nvidia-glx nvidia-kernel-`uname -r` nvidia-kernel-dkms nvidia-settings nvidia-xconfig
+nvidia-xconfig
