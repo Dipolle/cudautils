f1beb05a6c43ae56ac600c3dfec3625e81130962
Gurov D.A. <guranvirnew@yandex.ru>
2015-07-31 22:06:24 +0300

    Delete intel.sh

--- intel.sh
+++
@@ -1,3 +0,0 @@
-apt-get remove --purge xserver-xorg-video-intel
-wget https://download.01.org/gfx/ubuntu/14.04/main/pool/main/i/intel-linux-graphics-installer/intel-linux-graphics-installer_1.0.7-0intel1_amd64.deb
-dpkg -i *.deb
