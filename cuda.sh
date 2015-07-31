539f3ace846cfd2fc506cc611f22ca6b9275274d
Gurov D.A. <guranvirnew@yandex.ru>
2015-07-31 22:09:47 +0300

    Create cuda.sh

---
+++ cuda.sh
@@ -0,0 +1,2 @@
+wget http://us.download.nvidia.com/XFree86/Linux-x86_64/352.30/NVIDIA-Linux-x86_64-352.30.run
+wget http://developer.download.nvidia.com/compute/cuda/7_0/Prod/local_installers/cuda_7.0.28_linux.run
