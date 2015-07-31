b40e5eb10a6ed5983df508718794f987f042688a
Gurov D.A. <guranvirnew@yandex.ru>
2015-08-01 02:00:51 +0300

    Update debianscript.sh

--- debianscript.sh
+++ debianscript.sh
@@ -1,6 +1,6 @@
-  aptitude remove  --purge xserver-xorg-video-nouveau
   apt-get install freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libgl1-mesa-glx libglu1-mesa libglu1-mesa-dev
   aptitude install  xserver-xorg-video-intel
   apt-get install -y linux-headers-$(uname -r)
-  
+  apt-get remove  --purge xserver-xorg-video-nouveau
+
   
