e37177124b6a0ab3989b658677e752993079082a
Gurov D.A. <guranvirnew@yandex.ru>
2015-07-31 22:05:16 +0300

    Update debianscript.sh

--- debianscript.sh
+++ debianscript.sh
@@ -1,6 +1,6 @@
   aptitude remove  --purge xserver-xorg-video-nouveau
   apt-get install freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libgl1-mesa-glx libglu1-mesa libglu1-mesa-dev
-  echo "deb http://http.debian.net/debian wheezy-backports main">/etc/apt/sources.list.d/sources.list
-  apt-get update
-  aptitude -t wheezy-backports install "xserver-xorg-video-intel"
+  aptitude install  xserver-xorg-video-intel
+  apt-get install -y linux-headers-$(uname -r)
+  
   
